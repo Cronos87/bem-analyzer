7c2cf0f8dc6c6b77b8fb46d2270b170da851895d
Cronos87 <cronos87@hotmail.fr>
2015-06-23 22:51:34 +0200

    Fixed errors in the valid test

--- tests.py
+++ tests.py
@@ -9,11 +9,11 @@ class TestBEMAnalyzerMethods(unittest.TestCase):
                 <h5 class="article__title">Lorem ipsum dolor.</h5>
                 <div class="article__body">
                     Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sequi omnis dolorum, obcaecati minima quisquam, consectetur quibusdam, animi fugiat pariatur debitis quis quidem quos corporis odit porro cum modi beatae assumenda amet at eligendi inventore quia. Harum eligendi impedit quidem omnis ullam ipsum, delectus voluptates laudantium totam repellendus inventore iste hic.
-                    <a href="#" class="[ article__link  article__link--heavy ]  link">Lorem ipsum dolor sit amet, consectetur.</a>
+                    <a href="#" class="article__link article__link--heavy link">Lorem ipsum dolor sit amet, consectetur.</a>
                 </div>
                 <div class="article__tags">
                     <ul class="tag-list">
-                        <li class="tag-list__item  tag-list__item--first">lorem</li>
+                        <li class="tag-list__item tag-list__item--first">lorem</li>
                         <li class="tag-list__item">ipsum</li>
                         <li class="tag-list__item">dolor</li>
                     </ul>
@@ -31,7 +31,7 @@ class TestBEMAnalyzerMethods(unittest.TestCase):
                 <h5 class="article__title--h5">Lorem ipsum dolor.</h5>
                 <div class="article__body">
                     Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sequi omnis dolorum, obcaecati minima quisquam, consectetur quibusdam, animi fugiat pariatur debitis quis quidem quos corporis odit porro cum modi beatae assumenda amet at eligendi inventore quia. Harum eligendi impedit quidem omnis ullam ipsum, delectus voluptates laudantium totam repellendus inventore iste hic.
-                    <a href="#" class="article__body__link--heavy  link">Lorem ipsum dolor sit amet, consectetur.</a>
+                    <a href="#" class="article__body__link--heavy link">Lorem ipsum dolor sit amet, consectetur.</a>
                 </div>
                 <div class="article--tags">
                     <ul class="tag-list">
